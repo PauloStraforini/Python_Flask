0f60d476f870b93d8094a6ceaeac829d29bcc14d
PauloStraforini <paulostraforini@gmail.com>
2025-01-27 22:45:34 -0300

    ✨ Update: App.py

--- app.py
+++ app.py
@@ -1,7 +1,18 @@
 # Importação 
 from flask import Flask 
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///ecommerce.db'
+
+db = SQLAlchemy(app)
+
+#Modelagem de dados
+class Product(db.model): 
+    id = db.Columm(db.Integer, primary_key=True) 
+    name = db.Column(db.string(100), nullable=False)
+    prince = db.Column(db.Float, nullable=False)
+    description = db.Column(db.Text, nullable=True) 
 
 # Definição de rota raiz (Pagina Incial) e função que será executada ao requisitar 
 @app.route('/')
@@ -10,4 +21,3 @@ def Hello_Word():
 
 if __name__=="__main__":
     app.run(debug=True)
-    
