b49cce3c465aefd54d175467c152aa2620ed5ada
PauloStraforini <paulostraforini@gmail.com>
2025-01-28 23:03:23 -0300

    ✨ Update: Create "update"

--- app.py
+++ app.py
@@ -53,6 +53,28 @@ def get_products_details(product_id):
         })
     return jsonify ({"message": "Product not found"}), 404
 
+
+@app.route ('/api/products/update/<int:product_id>', methods=['PUT'])
+def update_product(product_id):
+    product = Product.query.get(product_id)
+    if not product: 
+        return jsonify ({"message": "Product not found"}), 404
+    
+    data = request.json
+    if 'name' in data: 
+        product.name = data['name']
+
+    if 'price' in data:
+        product.price = data['price']
+
+    if 'description' in data:
+        product.description = data['description']
+        
+
+    db.session.commit()
+    return jsonify ({"message": "Product update successfully"})
+
+
 # Definição de rota raiz (Pagina Incial) e função que será executada ao requisitar 
 @app.route('/')
 def Hello_Word(): 
